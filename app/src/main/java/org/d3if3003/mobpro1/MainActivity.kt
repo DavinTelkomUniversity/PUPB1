c2e664dc138fc00aa6677f7609d0a1ad38049770
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2024-02-29 13:26:25 +0700

    First

--- app/src/main/java/org/d3if3003/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3003/mobpro1/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.d3if3003.mobpro1.ui.theme.DavinTheme
-
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
