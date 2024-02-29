590aa258e307c3b6b18f9abf875292cbef2d1567
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2024-02-29 13:57:12 +0700

    Task 3.2.Membuat Tampilan Counter

--- app/src/main/java/org/d3if3003/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3003/mobpro1/MainActivity.kt
@@ -11,16 +11,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.d3if3003.mobpro1.ui.theme.DavinTheme
-
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.res.stringResource
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,36 +39,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("\n Nama : Davin Wahyu Wardana \n " +
-                            "Kelas : D3IF4603 \n " +
-                            "Nim : 6706223003")
+                    Counter()
                 }
             }
         }
     }
 }
 
-@Composable
-@ExperimentalMaterial3Api
-fun Greeting(name: String) {
-    MainScreen { modifier ->
-        Text(
-            text = name,
-            modifier = modifier,
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 // fun GreetingPreview() {
 fun ScreenPreview() {
     DavinTheme {
-        Greeting(" \n Davin Wahyu Wardana \n " +
-                " D3IF4603 \n " +
-                " 6706223003")
+        Counter()
     }
 }
 
@@ -80,5 +73,37 @@ fun MainScreen(content: @Composable (Modifier) -> Unit) {
         }
     ) { paddingValues ->
         content(Modifier.padding(paddingValues))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Counter() {
+    var number by remember { mutableIntStateOf(0) }
+    MainScreen { modifier ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                // text = "0",
+                text = number.toString(),
+                style = MaterialTheme.typography.displayLarge
+            )
+            Button(
+                // onClick = {},
+                onClick = { number++ },
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(top = 16.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                Text(text = stringResource(R.string.count))
+            }
+
+        }
     }
 }
