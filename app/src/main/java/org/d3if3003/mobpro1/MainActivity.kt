6ddb28a99f2fd524f548162fa93dc8b0b5563934
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2024-02-29 13:12:02 +0700

    Header

--- app/src/main/java/org/d3if3003/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3003/mobpro1/MainActivity.kt
@@ -1,5 +1,5 @@
 package org.d3if3003.mobpro1
-
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,7 +12,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.d3if3003.mobpro1.ui.theme.DavinTheme
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.TopAppBarDefaults
+
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +30,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Davin")
+                    Greeting("\n Nama : Davin Wahyu Wardana \n " +
+                            "Kelas : D3IF4603 \n " +
+                            "Nim : 6706223003")
                 }
             }
         }
@@ -30,17 +40,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+@ExperimentalMaterial3Api
+fun Greeting(name: String) {
+    MainScreen { modifier ->
+        Text(
+            text = name,
+            modifier = modifier,
+        )
+    }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun GreetingPreview() {
+// fun GreetingPreview() {
+fun ScreenPreview() {
     DavinTheme {
-        Greeting("Android")
+        Greeting(" \n Davin Wahyu Wardana \n " +
+                " D3IF4603 \n " +
+                " 6706223003")
+    }
+}
+
+@Composable
+@ExperimentalMaterial3Api
+fun MainScreen(content: @Composable (Modifier) -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { paddingValues ->
+        content(Modifier.padding(paddingValues))
     }
 }
