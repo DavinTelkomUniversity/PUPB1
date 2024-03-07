6800f5e5955995ffd806e23de21e7bf2b36125b9
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2024-03-07 13:18:06 +0700

    03-challenge

--- app/src/main/java/org/d3if3003/mobpro1/MainActivity.kt
+++ app/src/main/java/org/d3if3003/mobpro1/MainActivity.kt
@@ -23,46 +23,43 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+// DAVIN WAHYU WARDANA
+// 6706223003
+// D3IF-4603
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DavinTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Counter()
+                    LampuApp()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-// fun GreetingPreview() {
-fun ScreenPreview() {
-    DavinTheme {
-        Counter()
-    }
-}
+fun LampuApp() {
+    var lampuOn by remember { mutableStateOf(false) }
 
-@Composable
-@ExperimentalMaterial3Api
-fun MainScreen(content: @Composable (Modifier) -> Unit) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -75,53 +72,43 @@ fun MainScreen(content: @Composable (Modifier) -> Unit) {
                 )
             )
         }
-    ) { paddingValues ->
-        content(Modifier.padding(paddingValues))
-    }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun Counter() {
-    var number by remember { mutableIntStateOf(0) }
-    MainScreen { modifier ->
+    ) {
         Column(
-            modifier = modifier
+            modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
+            Image(
+                painter = if (lampuOn) painterResource(R.drawable.lampuon) else painterResource(R.drawable.lampuoff),
+                contentDescription = stringResource(R.string.gambar_lampu),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(132.dp)
+            )
+            Text(
+                text = if (lampuOn) "Lampu Hidup" else "Lampu Mati",
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Button(
+                onClick = { lampuOn = !lampuOn },
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(top = 24.dp),
+                contentPadding = PaddingValues(16.dp)
             ) {
-                Button(
-                    onClick = { if (number > 0) number-- },
-                    modifier = Modifier
-                        .padding(end = 30.dp)
-                        .size(50.dp)
-                        .background(color = MaterialTheme.colorScheme.secondary, shape = CircleShape),
-                    contentPadding = PaddingValues(0.dp)
-                ) {
-                    Text(text = "-")
-                }
-                Text(
-                    text = number.toString(),
-                    style = MaterialTheme.typography.displayLarge
-                )
-                Button(
-                    onClick = { number++ },
-                    modifier = Modifier
-                        .padding(start = 30.dp)
-                        .size(50.dp)
-                        .background(color = MaterialTheme.colorScheme.secondary, shape = CircleShape),
-                    contentPadding = PaddingValues(0.dp)
-                ) {
-                    Text(text = "+")
-                }
+                Text(text = if (lampuOn) "Matikan" else "Hidupkan")
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun LampuAppPreview() {
+    DavinTheme {
+        LampuApp()
     }
 }
