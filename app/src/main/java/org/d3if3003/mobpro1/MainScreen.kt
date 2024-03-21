044a2000d042697a249a6bfa9614aa414d7cda1b
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2024-03-21 11:19:15 +0700

    3.5.Menangani bug isian form hilang

--- app/src/main/java/org/d3if3003/mobpro1/MainScreen.kt
+++ app/src/main/java/org/d3if3003/mobpro1/MainScreen.kt
@@ -1,46 +1,54 @@
 package org.d3if3003.mobpro1
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import org.d3if3003.mobpro1.R
+import org.d3if3003.mobpro1.navigation.Screen
 import org.d3if3003.mobpro1.ui.theme.DavinTheme
 import kotlin.math.pow
 
@@ -73,34 +81,23 @@ fun MainScreen(navController: NavHostController) {
     }
 }
 
+@SuppressLint("StringFormatMatches")
 @Composable
-fun  ScreenContent(modifier: Modifier) {
-    var berat by remember {
-        mutableStateOf("")
-    }
-    var beratError by remember {
-        mutableStateOf(false)
-    }
-    var tinggi by remember {
-        mutableStateOf("")
-    }
-    var tinggiError by remember {
-        mutableStateOf(false)
-    }
-    val radioOptions = listOf(
-        stringResource(id = R.string.pria),
-        stringResource(id = R.string.wanita)
-    )
-    var gender by remember {
-        mutableStateOf(radioOptions[0])
-    }
-    var bmi by remember {
-        mutableStateOf(0f)
-    }
-    var kategori by remember {
-        mutableStateOf(0)
-    }
-    Column (
+fun ScreenContent(modifier: Modifier) {
+    var alas by rememberSaveable { mutableStateOf("") }
+    var alasError by rememberSaveable { mutableStateOf(false) }
+
+    var tinggi by rememberSaveable { mutableStateOf("") }
+    var tinggiError by rememberSaveable { mutableStateOf(false) }
+
+    var sisi by rememberSaveable { mutableStateOf("") }
+    var sisiError by rememberSaveable { mutableStateOf(false) }
+
+    var luas by rememberSaveable { mutableFloatStateOf(0f) }
+    var keliling by rememberSaveable { mutableFloatStateOf(0f) }
+
+    val context = LocalContext.current
+    Column(
         modifier = modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
@@ -108,73 +105,156 @@ fun  ScreenContent(modifier: Modifier) {
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(id = R.string.bmi_intro),
+        Text(
+            text = stringResource(id = R.string.segitiga_intro),
             style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.fillMaxWidth())
-        OutlinedTextField(value = berat, onValueChange = {berat = it},
-            label = { Text(text = stringResource(R.string.berat_badan))},
-            isError = beratError,
-            trailingIcon = { IconPicker(beratError, "kg")},
-            supportingText = { ErrorHint(beratError)},
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            ),
             modifier = Modifier.fillMaxWidth()
         )
-        OutlinedTextField(value = tinggi, onValueChange = {tinggi = it},
-            label = { Text(text = stringResource(R.string.Tinggi_badan))},
-            isError = tinggiError,
-            trailingIcon = { IconPicker(tinggiError, "cm")},
-            supportingText = { ErrorHint(tinggiError)},
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-        Row (
-            modifier = Modifier
-                .padding(top = 6.dp)
-                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
-        ) {
-            radioOptions.forEach{ text->
-                GenderOption(label = text, isSelected = gender == text , modifier = Modifier
-                    .selectable(
-
-
-                        selected = gender == text,
-                        onClick = { gender = text},
-                        role = Role.RadioButton
-                    )
-                    .weight(1f)
-                    .padding(16.dp)
+        Column {
+            OutlinedTextField(
+                value = alas,
+                onValueChange = {
+                    alas = it
+                },
+                label = { Text(text = "Alas") },
+                isError = alasError,
+                trailingIcon = { IconPicker(alasError, "cm") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.fillMaxWidth(),
+            )
+            if (alasError) {
+                Text(
+                    text = "Input alas tidak valid",
+                    color = Color.Red,
+                    modifier = Modifier.padding(start = 4.dp)
                 )
             }
         }
-        Button(onClick = {
-            beratError = (berat =="" || berat == "0")
-            tinggiError = (tinggi =="" || tinggi == "0")
-            if (beratError || tinggiError) return@Button
-            bmi = hitungBMI(berat.toFloat(), tinggi.toFloat())
-            kategori = getKategori(bmi, gender == radioOptions[0])
-        },
-            modifier = Modifier.padding(top = 8.dp),
-            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
-        ) {
-            Text(text = stringResource(R.string.hitung))
+
+        Column {
+            OutlinedTextField(
+                value = tinggi,
+                onValueChange = {
+                    tinggi = it
+                },
+                label = { Text(text = "Tinggi") },
+                isError = tinggiError,
+                trailingIcon = { IconPicker(tinggiError, "cm") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.fillMaxWidth(),
+            )
+            if (tinggiError) {
+                Text(
+                    text = "Input tinggi tidak valid",
+                    color = Color.Red,
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
         }
-        if (bmi != 0f) {
+
+        Column {
+            OutlinedTextField(
+                value = sisi,
+                onValueChange = {
+                    sisi = it
+                },
+                label = { Text(text = "Sisi") },
+                isError = sisiError,
+                trailingIcon = { IconPicker(sisiError, "cm") },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                modifier = Modifier.fillMaxWidth(),
+            )
+            if (sisiError) {
+                Text(
+                    text = "Input sisi tidak valid",
+                    color = Color.Red,
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = {
+                    alasError = (alas == "" || alas == "0")
+                    tinggiError = (tinggi == "" || tinggi == "0")
+                    sisiError = (sisi == "" || sisi == "0")
+                    if (alasError || tinggiError || sisiError) return@Button
+
+                    val calculatedLuas = hitungLuas(alas.toFloat(), tinggi.toFloat())
+                    val calculatedKeliling = hitungKeliling(sisi.toFloat(), sisi.toFloat(), sisi.toFloat())
+
+                    luas = calculatedLuas
+                    keliling = calculatedKeliling
+
+                },
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp),
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = stringResource(R.string.hitung))
+            }
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Button(
+                onClick = {
+                    alas = ""
+                    tinggi = ""
+                    sisi = ""
+                    luas = 0f
+                    keliling = 0f
+                },
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp),
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Reset")
+            }
+        }
+
+        if (luas != 0f || keliling != 0f) {
             Divider(
                 modifier = Modifier.padding(vertical = 8.dp),
                 thickness = 1.dp
             )
-            Text(text = stringResource(R.string.bmi_x, bmi),
-                style = MaterialTheme.typography.titleLarge)
-            Text(text = stringResource(kategori).uppercase(),
-                style = MaterialTheme.typography.headlineLarge)
+            Text(
+                text = "Luas: $luas",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Text(
+                text = "Keliling: $keliling",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Button(
+                onClick = {
+                    shareData(
+                        context = context,
+                        message = context.getString(R.string.bagikan_template,
+                            alas, tinggi, sisi, luas, keliling)
+                    )
+                },
+                modifier = Modifier.padding(top = 8.dp),
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+            ) {
+                Text(text = stringResource(R.string.bagikan))
+            }
         }
     }
 }
