b8e19d03fdf6abcf130f2b0d62492e917c7aad7d
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2024-03-21 12:45:59 +0700

    3.6.Membuat navigasi ke app lain

--- app/src/main/java/org/d3if3003/mobpro1/MainScreen.kt
+++ app/src/main/java/org/d3if3003/mobpro1/MainScreen.kt
@@ -40,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
@@ -111,6 +112,7 @@ fun  ScreenContent(modifier: Modifier) {
     var kategori by rememberSaveable {
         mutableStateOf(0)
     }
+    val context = LocalContext.current
     Column (
         modifier = modifier
             .fillMaxSize()
@@ -186,6 +188,19 @@ fun  ScreenContent(modifier: Modifier) {
                 style = MaterialTheme.typography.titleLarge)
             Text(text = stringResource(kategori).uppercase(),
                 style = MaterialTheme.typography.headlineLarge)
+            Button(
+                onClick = {
+                    shareData(
+                        context = context,
+                        message = context.getString(R.string.bagikan_template,
+                            berat, tinggi, gender, bmi)
+                    )
+                },
+                modifier = Modifier.padding(top = 8.dp),
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+            ) {
+                Text(text = stringResource(R.string.bagikan))
+            }
         }
     }
 }
