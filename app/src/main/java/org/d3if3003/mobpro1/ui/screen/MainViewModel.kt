e23f79792d887ef4868d2c0d0787004db8f02e70
DAVIN WAHYU WARDANA <davinww060304@gmail.com>
2024-03-28 13:51:29 +0700

    07-challenge

--- app/src/main/java/org/d3if3003/mobpro1/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if3003/mobpro1/ui/screen/MainViewModel.kt
@@ -11,7 +11,9 @@ class MainViewModel : ViewModel() {
         data.add(Mahasiswa("Muhammad Risqi Briliansyah", "6706223004", "D3IF-46-03"))
         data.add(Mahasiswa("Ghina Salsabilla Zulin", "6706223005", "D3IF-46-03"))
         data.add(Mahasiswa("Adhisty Nayyara Ramadhanti", "6706223006", "D3IF-46-03"))
-        data.add(Mahasiswa("Muhammad Faris Akbar", "6706223007", "D3IF-46-03"))
+        data.add(Mahasiswa("Raffi Ardiansyah", "6706223008", "D3IF-46-03"))
+        data.add(Mahasiswa("Didik Hermanto", "6706223009", "D3IF-46-03"))
+        data.add(Mahasiswa("Titik Sugiyarti", "6706223010", "D3IF-46-03"))
         return data
     }
 }
